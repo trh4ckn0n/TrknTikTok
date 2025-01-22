50fedf04b9335b5fc62e38c878209ae0cba04863
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2025-01-22 03:37:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,5 @@ def list_videos():
     return render_template('video_list.html', videos=videos)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
